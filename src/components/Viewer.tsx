18ce5cb245fb283a43dce1a0116b9cb189928449
kiplimoboor <kiplimoarapboor@gmail.com>
2025-10-02 16:07:01 +0300

    viewer update

--- src/components/Viewer.tsx
+++ src/components/Viewer.tsx
@@ -15,7 +15,10 @@ function Viewer() {
 
     const handleLoad = () => {
       if (iframe.contentWindow) {
-        iframe.contentWindow.postMessage({ type: "STUDY_DATA", payload: study }, "*");
+        iframe.contentWindow.postMessage(
+          { type: "STUDY_DATA", payload: study },
+          "https://radiology.mtrh.go.ke/editor/",
+        );
       }
     };
 
@@ -26,12 +29,14 @@ function Viewer() {
 
   return (
     <div className="flex h-screen w-full">
-      {/* <iframe */}
-      {/*   src={`https://radiology.mtrh.go.ke/ohif/viewer?StudyInstanceUIDs=${uid}`} */}
-      {/*   className={`h-full ${study?.radiologist ? "w-8/12" : "w-full"}`} */}
-      {/* /> */}
+      <iframe
+        src={`https://radiology.mtrh.go.ke/ohif/viewer?StudyInstanceUIDs=${uid}`}
+        className={`h-full ${study?.radiologist ? "w-8/12" : "w-full"}`}
+      />
 
-      {Boolean(study?.radiologist) && <iframe ref={iframeRef} src="http://127.0.0.1:5174/editor/" className="w-4/12" />}
+      {Boolean(study?.radiologist) && (
+        <iframe ref={iframeRef} src="https://radiology.mtrh.go.ke/editor/" className="w-4/12" />
+      )}
     </div>
   );
 }
