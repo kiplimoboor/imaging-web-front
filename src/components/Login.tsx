1c6fe26066a336c9887e46b5f9a94704906edf61
kiplimoboor <kiplimoarapboor@gmail.com>
2025-10-13 16:20:21 +0300

    logout even frappe

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,20 +1,20 @@
 function Login() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
-      <div className="text-center">
-        <h1 className="text-3xl sm:text-4xl md:text-5xl font-extrabold tracking-tight">MTRH Radiology Department</h1>
-        <p className="mt-4 text-lg sm:text-xl text-gray-700 max-w-xl mx-auto">Welcome to MTRH Radiology</p>
-        <div className="mt-8">
-          <a
-            href="/"
-            className="inline-block px-8 py-3 text-lg font-semibold text-white bg-blue-600 hover:bg-blue-700 transition-colors duration-300 "
-          >
-            Log in with ERPNext
-          </a>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+			<div className="text-center">
+				<h1 className="text-3xl sm:text-4xl md:text-5xl font-extrabold tracking-tight">MTRH Radiology Department</h1>
+				<p className="mt-4 text-lg sm:text-xl text-gray-700 max-w-xl mx-auto">Welcome to MTRH Radiology</p>
+				<div className="mt-8">
+					<a
+						href="/"
+						className="inline-block px-8 py-3 text-lg font-semibold text-white bg-blue-600 hover:bg-blue-700 transition-colors duration-300 "
+					>
+						Log in with ERPNext
+					</a>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Login;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,65 +1,68 @@
 import LogoutIcon from "@mui/icons-material/Logout";
 import { AppBar, Box, Button, Container, Toolbar, Typography } from "@mui/material";
-import { useNavigate } from "react-router";
 import { useAuth } from "../context/AuthContext";
 
 function Navbar() {
-  const { user } = useAuth();
-  const navigate = useNavigate();
+	const { user } = useAuth();
+	const handleLogout = async () => {
+		await fetch("https://radiology.mtrh.go.ke/oauth/revoke", { credentials: "include" });
+		localStorage.removeItem("token");
 
-  const handleLogout = async () => {
-    await fetch("https://radiology.mtrh.go.ke/oauth/revoke", { credentials: "include" });
-    localStorage.removeItem("token");
-    navigate("/login");
-  };
+		const frappeLogoutWindow = window.open("about:blank", "_blank", "width=100,height=100,top=0,left=0");
+		if (frappeLogoutWindow) {
+			frappeLogoutWindow.location.href = "https://portal.mtrh.go.ke/api/method/logout";
+			setTimeout(() => frappeLogoutWindow.close(), 200);
+		}
+		setTimeout(() => (window.location.href = "http://127.0.0.1:5173/login"), 500);
+	};
 
-  return (
-    <AppBar
-      position="static"
-      sx={{ backgroundColor: "white", color: "#AA0000", boxShadow: "none", borderBottom: "1px solid #e0e0e0" }}
-    >
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Box sx={{ display: "flex", alignItems: "center" }}>
-            <img src="/mtrh.svg" alt="MTRH Logo" />
-            <Typography
-              variant="h6"
-              noWrap
-              component="a"
-              sx={{
-                mr: 2,
-                display: { xs: "none", md: "flex" },
-                fontFamily: "monospace",
-                fontWeight: 700,
-                letterSpacing: ".3rem",
-                color: "inherit",
-                textDecoration: "none",
-              }}
-            >
-              MTRH
-            </Typography>
-          </Box>
-          <Box sx={{ flexGrow: 1 }} />
-          {user && (
-            <Box sx={{ display: "flex", alignItems: "center" }}>
-              <Typography variant="body2" sx={{ color: "black", marginRight: "6px" }}>
-                <strong>{user.full_name}</strong>
-              </Typography>
-              <>|</>
-              <Button
-                size="small"
-                startIcon={<LogoutIcon />}
-                sx={{ color: "black", textTransform: "capitalize", marginLeft: "6px" }}
-                onClick={handleLogout}
-              >
-                Log Out
-              </Button>
-            </Box>
-          )}
-        </Toolbar>
-      </Container>
-    </AppBar>
-  );
+	return (
+		<AppBar
+			position="static"
+			sx={{ backgroundColor: "white", color: "#AA0000", boxShadow: "none", borderBottom: "1px solid #e0e0e0" }}
+		>
+			<Container maxWidth="xl">
+				<Toolbar disableGutters>
+					<Box sx={{ display: "flex", alignItems: "center" }}>
+						<img src="/mtrh.svg" alt="MTRH Logo" />
+						<Typography
+							variant="h6"
+							noWrap
+							component="a"
+							sx={{
+								mr: 2,
+								display: { xs: "none", md: "flex" },
+								fontFamily: "monospace",
+								fontWeight: 700,
+								letterSpacing: ".3rem",
+								color: "inherit",
+								textDecoration: "none",
+							}}
+						>
+							MTRH
+						</Typography>
+					</Box>
+					<Box sx={{ flexGrow: 1 }} />
+					{user && (
+						<Box sx={{ display: "flex", alignItems: "center" }}>
+							<Typography variant="body2" sx={{ color: "black", marginRight: "6px" }}>
+								<strong>{user.full_name}</strong>
+							</Typography>
+							<>|</>
+							<Button
+								size="small"
+								startIcon={<LogoutIcon />}
+								sx={{ color: "black", textTransform: "capitalize", marginLeft: "6px" }}
+								onClick={handleLogout}
+							>
+								Log Out
+							</Button>
+						</Box>
+					)}
+				</Toolbar>
+			</Container>
+		</AppBar>
+	);
 }
 
 export default Navbar;
