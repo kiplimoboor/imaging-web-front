a3fa4d00a8dce9a65a3304d8c072cc5ffa628f28
kiplimoboor <kiplimoarapboor@gmail.com>
2025-10-31 12:26:12 +0300

    remove unused hooks

--- src/hooks/studies.ts
+++ src/hooks/studies.ts
@@ -1,7 +1,5 @@
 import { useMutation, useQuery } from "@tanstack/react-query";
-import { useMemo } from "react";
 import type { Study } from "@/types";
-import { useAuth } from "../context/AuthContext";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -32,19 +30,6 @@ function useGetStudies(filters?: GetStudiesFilters) {
 	});
 }
 
-function useUserStudies() {
-	const { user } = useAuth();
-	const { data: allStudies } = useGetStudies({});
-	const isRadiologist = user?.role === "System User" || user?.role === "Administrator";
-
-	const newStudies = useMemo(() => {
-		if (!allStudies) return [];
-		return allStudies.filter((study) => (isRadiologist ? study.radiologist === user?.id : study.student == user?.id));
-	}, [allStudies]);
-
-	return { data: newStudies };
-}
-
 type StudyUpdateFields = { patient_id?: string; student?: number; radiologist?: number; status?: number };
 type UpdateStudyPayload = { id: number; data: StudyUpdateFields };
 function useUpdateStudy() {
@@ -60,26 +45,4 @@ function useUpdateStudy() {
 	});
 }
 
-function useNewStudies() {
-	const { data: allStudies } = useGetStudies({ status: 0 });
-
-	const newStudies = useMemo(() => {
-		if (!allStudies) return [];
-		return allStudies.filter((study) => study.status === 0);
-	}, [allStudies]);
-
-	return { data: newStudies };
-}
-
-function useCompleteStudies() {
-	return useQuery<Study[]>({
-		queryKey: ["studies", "complete"],
-		queryFn: async (): Promise<Study[]> => {
-			const res = await fetch(API_URL + "/studies?status=4", { credentials: "include" });
-			const data: Study[] = await res.json();
-			return data;
-		},
-	});
-}
-
-export { useNewStudies, useGetStudies, useUserStudies, useCompleteStudies, useUpdateStudy, type Study };
+export { useGetStudies, useUpdateStudy };
