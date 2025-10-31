68576b95000f20136f677af33aeedc483d3053c2
kiplimoboor <kiplimoarapboor@gmail.com>
2025-10-31 12:41:56 +0300

    fix edit

--- src/components/studies/utils.ts
+++ src/components/studies/utils.ts
@@ -1,8 +1,10 @@
 import type { Study } from "@/types";
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 async function studyUpdate(id: number, values: Study) {
 	const { patient_id, patient_name, examination, dob, gender } = values;
-	await fetch("http://127.0.0.1:3000/studies/" + id, {
+	await fetch(API_URL + "/studies/" + id, {
 		method: "PATCH",
 		credentials: "include",
 		headers: { "Content-Type": "application/json" },
