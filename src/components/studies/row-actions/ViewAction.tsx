5e29b402a3030c1b961887aef5f025f943e14f41
kiplimoboor <kiplimoarapboor@gmail.com>
2025-11-11 11:49:16 +0300

    fix: wrong viewer url

--- src/components/studies/row-actions/ViewAction.tsx
+++ src/components/studies/row-actions/ViewAction.tsx
@@ -7,7 +7,7 @@ function ViewAction({ dicomUid, status }: ViewActionProps) {
 	const { isGuest } = useAuth();
 	const handleOpen = () => {
 		if (status === 0 || (isGuest && status != 4))
-			window.open("https://radiology.mtrh.go.ke/ohif/viewer/StudyInstanceUIDs=" + dicomUid);
+			window.open("https://radiology.mtrh.go.ke/ohif/viewer?StudyInstanceUIDs=" + dicomUid);
 		else window.open("viewer/" + dicomUid);
 	};
 	return (
