431cd3ecbce7b23013971a15aaa34f2ee3d48681
kiplimoboor <kiplimoarapboor@gmail.com>
2025-11-11 11:20:36 +0300

    open editor in guest only when complete

--- src/components/studies/row-actions/ViewAction.tsx
+++ src/components/studies/row-actions/ViewAction.tsx
@@ -1,14 +1,11 @@
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import { IconButton, Tooltip } from "@mui/material";
-import { useAuth } from "@/context/AuthContext";
 
-const VIEWER_URL = "https://radiology.mtrh.go.ke/ohif/viewer?StudyInstanceUIDs=";
 type ViewActionProps = { dicomUid: string; status: number };
 function ViewAction({ dicomUid, status }: ViewActionProps) {
-	const { isGuest } = useAuth();
 	const handleOpen = () => {
-		if (status === 0 || (isGuest && status != 4)) window.open(VIEWER_URL + dicomUid);
-		else window.open("viewer/" + dicomUid);
+		if (status === 4) window.open("viewer/" + dicomUid);
+		else window.open("https://radiology.mtrh.go.ke/ohif/viewer?StudyInstanceUIDs=" + dicomUid);
 	};
 	return (
 		<IconButton onClick={handleOpen}>
