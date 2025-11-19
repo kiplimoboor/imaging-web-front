89a8061eacd99ffe2ca2e989ad6c88364d237e34
kiplimoboor <kiplimoarapboor@gmail.com>
2025-11-19 16:23:50 +0300

    better naming for template

--- src/components/editor/editor-actions/ChangeTemplate.tsx
+++ src/components/editor/editor-actions/ChangeTemplate.tsx
@@ -4,12 +4,13 @@ import { Editor } from "@tiptap/react";
 import templates from "../data/templates";
 
 type ChangeTemplateProps = { editor: Editor };
+
 function ChangeTemplate({ editor }: ChangeTemplateProps) {
-	const handleChangeTemplate = (newValue: string | null) => {
+	const addTemplate = (template: string | null) => {
 		if (!editor) return;
-		if (newValue !== null) {
-			if (editor?.getText() === "") editor.commands.setContent(templates[newValue]);
-			else editor?.commands.setContent(editor.getHTML() + "<br>" + templates[newValue]);
+		if (template !== null) {
+			if (editor?.getText() === "") editor.commands.setContent(templates[template]);
+			else editor?.commands.setContent(editor.getHTML() + "<br>" + templates[template]);
 		} else editor?.commands.setContent("");
 	};
 
@@ -18,7 +19,7 @@ function ChangeTemplate({ editor }: ChangeTemplateProps) {
 			options={Object.keys(templates)}
 			disablePortal
 			sx={{ width: 300 }}
-			onChange={(_, newValue: string | null) => handleChangeTemplate(newValue)}
+			onChange={(_, newValue: string | null) => addTemplate(newValue)}
 			renderInput={(params) => <TextField {...params} label="Choose a Template" size="small" sx={{ height: 40 }} />}
 		/>
 	);
