13061827a00ff688909e81fc50687300d3603833
kiplimoboor <kiplimoarapboor@gmail.com>
2026-02-08 14:06:04 +0300

    open all in new orthanc

--- src/components/studies/row-actions/ViewAction.tsx
+++ src/components/studies/row-actions/ViewAction.tsx
@@ -4,23 +4,26 @@ import { useAuth } from "@/context/AuthContext";
 
 type ViewActionProps = { dicomUid: string; status: number, date: string };
 function ViewAction({ dicomUid, status, date }: ViewActionProps) {
+	console.log(date)
 	const { isGuest } = useAuth();
 	const handleOpen = () => {
 		if (status === 0 || (isGuest && status != 4)) {
-			// Previous Orthanc Crashed on this date. The code is messy, will refactor
-			if (Number(date) > 20260205) {
-				window.open("https://radiology.mtrh.go.ke/viewer-v1/viewer?StudyInstanceUIDs=" + dicomUid);
-			} else {
-				window.open("https://radiology.mtrh.go.ke/ohif/viewer?StudyInstanceUIDs=" + dicomUid);
-			}
+			window.open("https://radiology.mtrh.go.ke/viewer-v1/viewer?StudyInstanceUIDs=" + dicomUid);
+			// Previous orthanc Crashed on this date. The code is messy, will refactor
+			// if (Number(date) > 20260205) {
+			// 	window.open("https://radiology.mtrh.go.ke/viewer-v1/viewer?StudyInstanceUIDs=" + dicomUid);
+			// } else {
+			// 	window.open("https://radiology.mtrh.go.ke/ohif/viewer?StudyInstanceUIDs=" + dicomUid);
+			// }
 		}
 		else {
-			if (Number(date) > 20260205) {
-				window.open(`viewer?viewer=viewer-v1&uid=${dicomUid}`);
-			}
-			else {
-				window.open(`viewer?viewer=ohif&uid=${dicomUid}`);
-			}
+			window.open(`viewer?viewer=viewer-v1&uid=${dicomUid}`);
+			// if (Number(date) > 20260205) {
+			// 	window.open(`viewer?viewer=viewer-v1&uid=${dicomUid}`);
+			// }
+			// else {
+			// 	window.open(`viewer?viewer=ohif&uid=${dicomUid}`);
+			// }
 		}
 	};
 	return (
