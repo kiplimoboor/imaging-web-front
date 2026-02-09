f5a2a1c686117285ffdd9c0992caa093fcca724b
kiplimoboor <kiplimoarapboor@gmail.com>
2026-02-09 12:33:24 +0300

    date update

--- src/components/studies/row-actions/ViewAction.tsx
+++ src/components/studies/row-actions/ViewAction.tsx
@@ -2,17 +2,18 @@ import VisibilityIcon from "@mui/icons-material/Visibility";
 import { IconButton, Tooltip } from "@mui/material";
 import { useAuth } from "@/context/AuthContext";
 
-type ViewActionProps = { dicomUid: string; status: number, date: string };
+type ViewActionProps = { dicomUid: string; status: number; date: string };
 function ViewAction({ dicomUid, status, date }: ViewActionProps) {
 	const { isGuest } = useAuth();
 
-	const archived = Number(date) < 20260205 // Orthanc that crashed and is now archived with images before this date
+	const archived = Number(date) < 20260206; // Orthanc that crashed and is now archived with images before this date
 
 	const handleOpen = () => {
 		if (status === 0 || (isGuest && status != 4)) {
-			window.open(`https://radiology.mtrh.go.ke/images${archived ? "-archive" : ""}/viewer?StudyInstanceUIDs=${dicomUid}`)
-		}
-		else {
+			window.open(
+				`https://radiology.mtrh.go.ke/images${archived ? "-archive" : ""}/viewer?StudyInstanceUIDs=${dicomUid}`,
+			);
+		} else {
 			window.open(`viewer?viewer=images${archived ? "-archive" : ""}&uid=${dicomUid}`);
 		}
 	};
@@ -26,3 +27,4 @@ function ViewAction({ dicomUid, status, date }: ViewActionProps) {
 }
 
 export default ViewAction;
+
